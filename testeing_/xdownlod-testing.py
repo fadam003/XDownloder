bbe8e5da16419c2b4e21d57dc0cb56aba5e26a0e
فاطمة آدم <45648319+fadam003@users.noreply.github.com>
2019-01-07 02:21:18 -0800

    Update xdownlod-testing.py

--- testeing_/xdownlod-testing.py
+++ testeing_/xdownlod-testing.py
@@ -1,9 +1,9 @@
 import pafy
 from hurry.filesize import size
-video = pafy.new('https://www.youtube.com/watch?v=B_VeP1S-T8Q&start_radio=1&list=RDB_VeP1S-T8Q')
+video = pafy.new(Youtubeurl)
 
 print(video.title)
 print(video.thumb)
 streams = video.streams
 for s in streams:
-    print(s.get_filesize())
+    print(size(s.get_filesize())) #gives us the size of the videos in M
